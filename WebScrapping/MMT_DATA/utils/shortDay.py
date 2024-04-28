94f6136ba3939c75186a806b5f4fd26a1f3a382a
gaurav <gauravy1905@gmail.com>
2024-04-28 17:06:32 +0530

    Added some utility functions for webscrapping

---
+++ WebScrapping/MMT_DATA/utils/shortDay.py
@@ -0,0 +1,12 @@
+def Day_converter(day:str) -> str:
+    days = {
+        'Monday':'Mon',
+        'Tuesday':'Tue',
+        'Wednesday':'Wed',
+        'Thursday':'Thu',
+        'Friday':'Fri',
+        'Saturday':'Sat'
+    }
+
+    return days[day]
+
