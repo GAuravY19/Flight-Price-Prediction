e7130ef33d40a9fb97524e95230fd303d4a83f85
gaurav <gauravy1905@gmail.com>
2024-04-29 11:18:43 +0530

    EaseMyTrip One way scrapper completed

---
+++ WebScrapping/goibibo_scrapper/utils/ShortMonth.py
@@ -0,0 +1,17 @@
+def Month_convert(month:str):
+    Month = {
+        'January':'Jan',
+        "Feburary":'Feb',
+        "March":'Mar',
+        "April":'Apr',
+        "May":'May',
+        "June":'Jun',
+        "July":'Jul',
+        "August":'Aug',
+        "September":'Sep',
+        "October":'Oct',
+        "November":'Nov',
+        "December":'Dec'
+    }
+
+    return Month[month]
